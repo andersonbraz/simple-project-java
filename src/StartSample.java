826ac5a331968ae7f3b24bccb863970f7a2fb2ac
Anderson Braz <braz_anderson@msn.com>
2020-08-20 11:26:55 -0300

    Up Files

---
+++ src/StartSample.java
@@ -0,0 +1,15 @@
+import br.sample.Client;
+
+class StartSample {
+	public static void main(String[] args) {
+		
+		Client client = new Client();
+		
+		client.firstName = "Anderson";
+		client.lastName = "Sousa";
+		client.email = "contato@andersonbraz.com";
+		
+		System.out.println("Hello Git!");
+		
+	}
+}
---
+++ src/br/sample/Client.java
@@ -0,0 +1,12 @@
+package br.sample;
+
+public class Client {
+	
+	public String firstName;
+	public String lastName;
+	public String email;
+	
+	void singup() {
+		System.out.println("Client - Signup");
+	}
+}
